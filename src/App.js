fd1550af314e698bdfd24dc2f276ed0e0b2db1c6
Nikita Bonachev <nbonachev@gmail.com>
2017-02-09 00:17:10 +0500

    add: logic prototype

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { Message } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
-  render() {
+  //noinspection JSMethodCanBeStatic
+	render() {
     return (
       <div className="main ui text container">
         <h1 className="ui dividing centered header">Mining Calculator</h1>
@@ -18,20 +19,109 @@ class App extends Component {
 
 export default App;
 
-const list = [
-	'Окупаемость оборудования через 3 дня',
-	'Прибыль в день 10$',
-];
+class MessageResult extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hidden: true,
+			list: [],
+			header: '',
+			icon: 'warning'
+		};
+	};
+	componentWillReceiveProps(props) {
+	  if (props.result.type !== 'no') {
+			this.setState({
+				header: props.result.type  === 'good' ? 'Результат прибыльный' : 'Результат убыточный',
+				list: [
+					`Прибыль в день ${props.result.profitInDay} $`,
+					`Окупаемость оборудования через ${props.result.profitFromCardInDays}`
+				],
+				icon: props.result.type === 'good' ? 'thumbs up' : 'warning',
+				hidden: props.result.type === 'no'
+			});
+    }
+  };
+  render() {
+    if (this.props.result.type === 'good') {
+			return (
+        <Message
+          hidden={this.state.hidden}
+          attached='bottom'
+          list={this.state.list}
+          header={this.state.header}
+          icon={this.state.icon}
+          success />
+			)
+    } else {
+			return (
+        <Message
+          hidden={this.state.hidden}
+          attached='bottom'
+          list={this.state.list}
+          header={this.state.header}
+          icon={this.state.icon}
+          error />
+			)
+    }
 
-const FormExampleForm = React.createClass({
-  getInitialState: function () {
-    return {
+	};
+}
+
+class FormExampleForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
 			messageHidden: false,
-    };
-  },
-  handleDismiss: function() {
+      electricityCostPerHour: '',
+      cryptoCurrencyCourse: '',
+      videoCardEnergyPerHour: '',
+      videoCardCost: '',
+      videoCardNumber: '',
+			videoCardProfit: '',
+			result: {
+			  type: 'no'
+      }
+		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	};
+	handleSubmit(event) {
+		event.preventDefault();
+		let energyCostPermHour = this.state.electricityCostPerHour;
+		let cryptoCurrencyCourse = this.state.cryptoCurrencyCourse;
+		let videoCardEnergyPerHour = this.state.videoCardEnergyPerHour;
+		let videoCardCost = this.state.videoCardCost;
+		let videoCardNumber = this.state.videoCardNumber;
+		let videoCardProfit = this.state.videoCardProfit;
+
+		let energyCostInDollars = energyCostPermHour / 60;
+		let coinsInDay = videoCardProfit * videoCardNumber * 24;
+		let moneyForEnergyPerDay = videoCardEnergyPerHour * 24 * energyCostInDollars;
+
+		let profitInDay = coinsInDay * cryptoCurrencyCourse - moneyForEnergyPerDay; // Прибыль карты в день
+		let profitFromCardInDays = videoCardCost / profitInDay; // Окупаемость карты через дней
+
+    this.setState({
+      result: {
+        type: profitFromCardInDays > 100 ? 'bad' : 'good',
+				profitInDay,
+				profitFromCardInDays
+      }
+    });
+	};
+	handleInputChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	}
+	handleDismiss() {
 	  this.setState({ messageHidden: true });
-  },
+  };
   render() {
     return (
       <div>
@@ -43,45 +133,76 @@ const FormExampleForm = React.createClass({
           <Message.Header>Хотите подсчитать свою прибыль с указанным оборудованием?</Message.Header>
           <p>Заполните все указанные ниже поля</p>
         </Message>
-        <Form className='attached fluid segment'>
+        <Form
+          className='attached fluid segment'
+          onSubmit={this.handleSubmit}
+        >
           <Header size='small'>Общие параметры</Header>
           <Form.Field>
             <label>Стоимость электроэнергии ($/кВт.ч.)</label>
-            <input placeholder='0'/>
+            <input
+              name='electricityCostPerHour'
+              placeholder='0'
+              value={this.state.electricityCostPerHour}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
           <Form.Field>
             <label>Курс добываем криптовалюты ($)</label>
-            <input placeholder='0'/>
+            <input
+              name='cryptoCurrencyCourse'
+              placeholder='0'
+              value={this.state.cryptoCurrencyCourse}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
           <Divider section/>
           <Header size='small'>Оборудование</Header>
           <Form.Field>
             <label>Потребление энергии (кВт./ч.)</label>
-            <input placeholder='0'/>
+            <input
+              name='videoCardEnergyPerHour'
+              placeholder='0'
+              value={this.state.videoCardEnergyPerHour}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
           <Form.Field>
             <label>Стоимость ($)</label>
-            <input placeholder='0'/>
+            <input
+              name='videoCardCost'
+              placeholder='0'
+              value={this.state.videoCardCost}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
           <Form.Field>
             <label>Количество единиц оборудования</label>
-            <input placeholder='0'/>
+            <input
+              name='videoCardNumber'
+              placeholder='0'
+              value={this.state.videoCardNumber}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
-          <Button type='submit'>Рассчитать</Button>
+          <Form.Field>
+            <label>Валюты в час</label>
+            <input
+              name='videoCardProfit'
+              placeholder='0'
+              value={this.state.videoCardProfit}
+              onChange={this.handleInputChange}
+            />
+          </Form.Field>
+          <Button
+            type='submit'
+            content='Рассчитать'
+          />
         </Form>
-        <Message
-          attached='bottom'
-          list={list}
-          header='Результат убыточный'
-          icon='warning'
-          error />
-        <Message
-          attached='bottom'
-          list={list}
-          header='Результат прибыльный'
-          icon='thumbs up'
-          success />
+        <MessageResult
+          result={this.state.result}
+        />
       </div>
     )
-  }
-});
+  };
+}
